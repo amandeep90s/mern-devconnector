4beb6208a34b78d75398d75f7bb2fde6da8e0472
aman <programmeramandeep@gmail.com>
2021-06-30 23:10:42 +0530

    spiiner, textareafieldgroup, textfieldgroup added

--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -1,7 +1,16 @@
 import React from "react";
+import spinner from "./spinner.gif";
 
 const Spinner = () => {
-    return <div></div>;
+    return (
+        <div>
+            <img
+                src={spinner}
+                style={{ width: "200px", margin: "auto", display: "block" }}
+                alt="Loading..."
+            />
+        </div>
+    );
 };
 
 export default Spinner;
