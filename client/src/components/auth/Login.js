e176e3d35d507c6d627d74597a9ad56d1191d76d
aman <programmeramandeep@gmail.com>
2021-07-02 18:19:48 +0530

    added nav links

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -31,7 +31,7 @@ const Login = ({ history }) => {
         <div className="login mb-5">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-8 m-auto">
+                    <div className="col-md-6 m-auto">
                         <h1 className="display-4 text-center">Log In</h1>
                         <p className="lead text-center">
                             Sign in to your DevConnector account
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 const Navbar = () => {
+    const dispatch = useDispatch();
+    const { auth } = useSelector((state) => ({ ...state }));
+
+    const { isAuthenticated, user } = auth;
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        dispatch(clearCurrentProfile());
+        dispatch(logoutUser());
+    };
+
+    const authLinks = (
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <Link className="nav-link" to="/feed">
+                    Post Feed
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/dashboard">
+                    Dashboard
+                </Link>
+            </li>
+            <li className="nav-item">
+                <a href="/#" onClick={handleLogout} className="nav-link">
+                    <img
+                        src={user.avatar}
+                        alt={user.name}
+                        className="rounded-circle"
+                        style={{ width: "2rem", marginRight: "0.5rem" }}
+                        title="You must have a Gravatar connected to your email to display an image"
+                    />{" "}
+                    Logout
+                </a>
+            </li>
+        </ul>
+    );
+
+    const guestLinks = (
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                    Register
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                    Login
+                </Link>
+            </li>
+        </ul>
+    );
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
             <div className="container">
@@ -30,18 +86,7 @@ const Navbar = () => {
                         </li>
                     </ul>
 
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">
-                                Register
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">
-                                Login
-                            </Link>
-                        </li>
-                    </ul>
+                    {isAuthenticated ? authLinks : guestLinks}
                 </div>
             </div>
         </nav>
