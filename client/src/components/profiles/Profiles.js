297f323f3d86d1538800cbb1c36fb8938ef66e22
aman <programmeramandeep@gmail.com>
2021-07-03 10:53:55 +0530

    profiles and profileItem component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProfileItem from "./ProfileItem";
+import { getProfiles } from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
 
 const Profiles = () => {
-    return <div></div>;
+    const dispatch = useDispatch();
+    const {
+        profile: { loading, profiles },
+    } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        dispatch(getProfiles());
+    }, [dispatch]);
+
+    let profileItems;
+    if (profiles === null || loading) {
+        profileItems = <Spinner />;
+    } else if (profiles.length > 0) {
+        profileItems = profiles.map((profile) => (
+            <ProfileItem key={profile._id} profile={profile} />
+        ));
+    } else {
+        profileItems = <h4>No profiles found...</h4>;
+    }
+
+    return (
+        <div className="row">
+            <div className="col-md-12">
+                <h4 className="text-center">Developer Profiles</h4>
+                <p className="lead text-center">
+                    Browse and connect with developers
+                </p>
+
+                {profileItems}
+            </div>
+        </div>
+    );
 };
 
 export default Profiles;
