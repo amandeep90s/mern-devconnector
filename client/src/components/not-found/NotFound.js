36278d4da4a9475f9070fcfcf687e8927bd26512
aman <programmeramandeep@gmail.com>
2021-07-03 10:30:25 +0530

    page not found

--- client/src/components/not-found/NotFound.js
+++ client/src/components/not-found/NotFound.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return <div></div>;
+    return (
+        <div className="text-center">
+            <h1 className="display-4">Page Not Found</h1>
+            <p>
+                Sorry, this page does not exist,{" "}
+                <Link to="/dashboard">Go Back</Link>
+            </p>
+        </div>
+    );
 };
 
 export default NotFound;
